5f27fbcf6d12c41927265e80c73aee0092e4dc1b
TangHanF <guofu_gh@163.com>
2018-09-06 17:54:47 +0800

    新增分栏式风格

--- js/component/vuecomponent.js
+++ js/component/vuecomponent.js
@@ -1,12 +1,12 @@
 Vue.component("form-field", {
     props: ['value', 'elestyle', 'selected']
 
-    ,computed:{
-        selected_model:function () {
+    , computed: {
+        selected_model: function () {
             return this.selected;
         }
     }
-    ,template: `
+    , template: `
     <form class="form-inline has-success">
                 <div class="form-group">
                     <input 
@@ -30,6 +30,55 @@ Vue.component("form-field", {
             </form>
     `
 });
+
+
+Vue.component("page-footer", {
+    template: `
+    <div class="jumbotron">
+    <p>欢迎使用</p>
+    <h1>Excel公式与函数帮助手册</h1>
+    <p>欢迎各位Star、Fork进行完善。时间有限，没做细致优化，有意向者可以一起完善</p>
+    <p>对外发布、提供服务请保留项目地址，谢谢合作！</p>
+    <strong class="text-danger">移动端访问时如果弹窗之后无法显示全部，请关闭弹窗再次打开（后续优化，提高兼容性）</strong>
+
+    <p class="text-danger">详情阅读：
+        <a href="https://github.com/TangHanF/excel_function_manual/blob/master/README.md" target="_blank">Readme</a>
+    </p>
+    <hr>
+    <blockquote>
+        <p><strong>作&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;者：</strong>GuoFu</p>
+        <p><strong>邮&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;箱：</strong>
+            <a href="mailto:guofu_gh@163.com" data-toggle="tooltip" data-placement="right" title="有问题欢迎邮件联系">guofu_gh@163.com</a>
+        </p>
+        <p><strong>创建日期：</strong>2018年09月05日</p>
+        <p><strong>GitHub地址：</strong><a href="https://github.com/TangHanF" target="_blank">https://github.com/TangHanF</a>
+        </p>
+        <p><strong>Gitee地址：</strong><a href="https://gitee.com/TangHanF"
+                                       target="_blank">https://gitee.com/TangHanF</a></p>
+    </blockquote>
+    <hr>
+    <h2>本项目可通过以下两个入口进行访问：</h2>
+    <p>
+        <strong>Gitee入口：</strong>
+        <a href="https://tanghanf.gitee.io/excel_function_manual/" target="_blank" data-toggle="tooltip"
+           data-placement="right" title="访问速度快">https://tanghanf.gitee.io/excel_function_manual/</a>
+    </p>
+    <p>
+        <strong>Github入口：</strong>
+        <a href="https://tanghanf.github.io/excel_function_manual/" target="_blank" data-toggle="tooltip"
+           data-placement="right" title="访问速度稍慢">https://tanghanf.github.io/excel_function_manual/</a>
+    </p>
+
+    <p>
+        <a class="btn btn-primary btn-lg" href="https://github.com/TangHanF/excel_function_manual" target="_blank"
+           role="button">本项目GitHub地址</a>
+        <a class="btn btn-primary btn-lg" href="https://gitee.com/TangHanF/excel_function_manual" target="_blank"
+           role="button">本项目Gitee地址</a>
+    </p>
+</div>
+    `
+})
+
 
 // 插槽
 Vue.component('alert-box', {
--- js/support.js
+++ js/support.js
@@ -17,26 +17,28 @@ let app2;
             app2 = new Vue({
                 el: "#app2",
                 data: {
-                    iframeSrc: "",
+                    iframeSrc: ""
                 }
             });
             app = new Vue({
                 el: "#app"
                 , data: {
-                    jsonData: searchFunNameJsonData,
-                    tmpJsonData: [],
-                    keyword: "",//搜索框的搜索关键字
-                    isShow: true,
-                    currentStyleName: '',//当前风格名称
-                    pagestyle: [
-                        {name: '详情折叠式', value: 'index.html'},
-                        {name: '详情弹窗式', value: 'index2.html'}
-                    ],
-                    currentSelected: 'index.html',
-                    iframeSrc: "",
-                    renderIframeXiangxiFlagArr: {},
-                    panel: 'panel',
-                    panelStyle: ['panel-primary', 'panel-success', 'panel-info', 'panel-warning', 'panel-danger'],
+                    jsonData: searchFunNameJsonData
+                    , tmpJsonData: []
+                    , keyword: ""//搜索框的搜索关键字
+                    , isShow: true
+                    , currentStyleName: ''//当前风格名称
+                    , pagestyle: [
+                        {name: '详情折叠式', value: 'index.html'}
+                        , {name: '详情弹窗式', value: 'index2.html'}
+                        , {name: '详情分栏式', value: 'index3.html'}
+                    ]
+                    , currentSelected: 'index.html'
+                    , iframeSrc: ""
+                    , renderIframeXiangxiFlagArr: {}
+                    , panel: 'panel'
+                    , panelStyle: ['panel-primary', 'panel-success', 'panel-info', 'panel-warning', 'panel-danger']
+                    , sectionsChildren: []
                 }
                 , methods: {
                     itemClick: function (href, e) {
@@ -44,28 +46,39 @@ let app2;
                         app2.iframeSrc = href;
                         console.log(href);
                         $("#myModal").modal({})
-                    },
-                    itemClick2: function (href, e) {
+                    }
+                    , itemClick2: function (href, e) {
                         //由于 JavaScript 的限制，Vue 不能检测对象属性的添加、删除、更新，可使用Vue.set进行更新
                         Vue.set(app.renderIframeXiangxiFlagArr, href, true);
                         console.log(href);
-                    },
+                    }
                     // 生成随机样式
-                    randomPanelClass: function () {
+                    , randomPanelClass: function () {
                         return this.panelStyle[Math.floor(Math.random() * this.panelStyle.length)];
-                    },
-                    selectChange: function (ele) {
+                    }
+                    , selectChange: function (ele) {
                         let selected = ele.target.value;
-                        this.currentStyleName = selected;
+                        app.currentStyleName = selected;
                         window.location.href = selected;
-                    },
-                    getDes: function (data) {
+                    }
+                    , getDes: function (data) {
                         return data.substr(data.indexOf(")") + 1);
 
-                    },
-                    getFormat: function (data) {
+                    }
+                    , getFormat: function (data) {
                         return data.substr(0, data.indexOf(")") + 1);
 
+                    }
+                    , style3LinkClick: function (key) {
+                        app.sectionsChildren = [];
+                        // 获取当前点击的一级分类下的二级分类集合
+                        let second = searchFunNameJsonData.sections[key].children;
+                        $.each(second, function (index, childrenKey) {
+                            // 获取childrenKey所对应的描述信息
+                            let topics = searchFunNameJsonData.topics[childrenKey];
+                            // 由于 JavaScript 的限制，Vue 不能检测以下变动的数组,可使用Vue.set进行更新
+                            Vue.set(app.sectionsChildren, index, topics);
+                        })
                     }
                 },
                 computed: {
@@ -110,6 +123,9 @@ let app2;
                 , mounted: function () {
                     this.currentSelected = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
                     console.log("===:" + this.currentSelected)
+                    $("[data-toggle='tooltip']").tooltip();
+                }
+                , updated: function () {
                     $("[data-toggle='tooltip']").tooltip();
                 }
 
